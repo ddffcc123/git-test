103f4bef5952a8ea041b95a30c12e50c54b85cbe
dfc <2495900030@qq.com>
2022-04-05 12:52:05 +0800

    hot-fix commit1

--- src/main/java/com/dfc/git/GitTest.java
+++ src/main/java/com/dfc/git/GitTest.java
@@ -3,5 +3,8 @@ package com.dfc.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git1!");
+        System.out.println("hello git2!");
+        System.out.println("hello git2!  hot-fix");
     }
 }
